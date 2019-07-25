c6df6e9201940fda8c41632823e284c85bd9f790
noth8 <denys@devmeed.com>
2019-07-25 10:34:55 +0200

    Add redux store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 import { StatusBar } from "./components/common";
 import { HomeScreen } from "./screens/";
+
+const store = createStore(reducers, applyMiddleware(ReduxThunk));
 
 class App extends Component {
   render() {
     return (
-      <View>
+      <Provider store={store}>
         <StatusBar />
         <HomeScreen />
-      </View>
+      </Provider>
     );
   }
 }
