ed222305953c0b1d2bd11d65ab9decf908553db6
noth8 <denys@devmeed.com>
2019-07-29 13:05:37 +0200

    Add firebase support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import firebase from "@firebase/app";
+import "@firebase/auth";
 import { createStore, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
+import config from "./config/firebase";
 import { StatusBar } from "./components/common";
 import { AppNavigator, ReactNavigationRedux } from "./navigation/AppNavigator";
 
@@ -13,6 +15,12 @@ const store = createStore(
 );
 
 class App extends Component {
+  componentDidMount() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+  }
+
   render() {
     return (
       <Provider store={store}>
---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDLqHtDtlxxCW08UkYBzSmj0LxO-_0rkCg",
+  authDomain: "gulsnap.firebaseapp.com",
+  databaseURL: "https://gulsnap.firebaseio.com",
+  projectId: "gulsnap",
+  storageBucket: "",
+  messagingSenderId: "744186948223",
+  appId: "1:744186948223:web:42f93dbea9acf176"
+};
